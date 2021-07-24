39dbc34f395ab6bb95f01effa84ed7140062724b
Pardeep-Kohli <pardeepkohli12@gmail.com>
2021-07-24 13:16:32 +0530

    formatting

--- Result.c
+++ Result.c
@@ -5,32 +5,42 @@ int main()
     float sub1, sub2, sub3, sub4, sub5, sub6;
     printf("How Many Subject Do You Have? : ");
     scanf("%d", &many);
-    if (many > 0){
+    if (many > 0)
+    {
         printf("Enter Your Subject with Mark( Eg. Hindi 40) : ");
-    scanf("%f", &sub1);
-    many--;}
-    if (many > 0){
+        scanf("%f", &sub1);
+        many--;
+    }
+    if (many > 0)
+    {
         printf("Enter Your Subject with Mark( Eg. Hindi 40) : ");
-    scanf("%f", &sub1);
-    many--;}
-    if (many > 0){
+        scanf("%f", &sub1);
+        many--;
+    }
+    if (many > 0)
+    {
         printf("Enter Your Subject with Mark( Eg. Hindi 40) : ");
-    scanf("%f", &sub1);
-    many--;}
-    if (many > 0){
+        scanf("%f", &sub1);
+        many--;
+    }
+    if (many > 0)
+    {
         printf("Enter Your Subject with Mark( Eg. Hindi 40) : ");
-    scanf("%f", &sub1);
-    many--;}
-    if (many > 0){
+        scanf("%f", &sub1);
+        many--;
+    }
+    if (many > 0)
+    {
         printf("Enter Your Subject with Mark( Eg. Hindi 40) : ");
-    scanf("%f", &sub1);
-    many--;}
-    if (many > 0){
+        scanf("%f", &sub1);
+        many--;
+    }
+    if (many > 0)
+    {
         printf("Enter Your Subject with Mark( Eg. Hindi 40) : ");
-    scanf("%f", &sub1);
-    many--;}
-    
-
+        scanf("%f", &sub1);
+        many--;
+    }
 
     return 0;
 }
